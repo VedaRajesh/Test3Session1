f6ea1b88adc2a7b3e0c4124f11d0f4c46022db9e
Veda R <94687727+VedaRajesh@users.noreply.github.com>
2022-02-28 06:31:49 +0000

    28.1.2022

--- p1original.c
+++ p1original.c
@@ -1,2 +1,29 @@
-
+#include<stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+ printf("enter the first numerator\n");
+    scanf("%d",num1);
+    printf("enter the first dinominator");
+        scanf("%d",den1);
+    printf("enter the second numerator\n");
+    scanf("%d",num2);
+    printf("enter the second numerator\n");
+    scanf("%d",den2);
+}
+void add(int num1,int den1, int num2, int den2, 
+int *num3, int *den3)
+{
+    *num3/*den3=(num1/den1)+(num2/den2);
+}
+void output(int num1, int den1, int num2, int den2, int num3, int den3)
+{
+    printf("sum is %d/%d",num3,den3);
+}
+int main()
+{
+    int  num1,den1,num2,den2,num3,den3;
+    input(&num1,&den1,&num2,&den2);
+    add(num1,den1,num2,den2,&num3,&den3);
+    output(num1,den1,num2,den2,num3,den3);
+    return 0;
 }
