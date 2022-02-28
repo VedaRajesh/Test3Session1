71121eabba9f0e3f17084df7a4f20b8ecb482043
Veda R <94687727+VedaRajesh@users.noreply.github.com>
2022-02-28 06:58:35 +0000

    28.feb.2022

--- p1original.c
+++ p1original.c
@@ -13,7 +13,7 @@ void input(int *num1, int *den1, int *num2, int *den2)
 void add(int num1,int den1, int num2, int den2, 
 int *num3, int *den3)
 {
-    *num3=(num1*den2)+(num2*den2);
+    *num3=(num1*den1)+(num2*den2);
     *den3=den1*den2;
 }
 void output(int num1, int den1, int num2, int den2, int num3, int den3)
